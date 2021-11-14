2831515bfaba47395d53d8210285f641a3d6e3b6
Eric <ngirabacu.eric@gmail.com>
2021-11-14 14:19:01 +0100

    Planning for CRUD methods

--- app.js
+++ app.js
@@ -2,9 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/**
+ * Clients 
+ */
+
+// Retrieve one client
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// Retrieve all clients
+
+// Add one client
+
+// Edit one client 
+
+/**
+ * Requests 
+ */
+
+// Retrieve one request
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Retrieve all requests
+
+// Add one request
+
+// Edit one request
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
